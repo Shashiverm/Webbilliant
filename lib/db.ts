7925ead5c055da6bdcccdd979eefae14ac74cf50
Shashiverm <meetshashikantkumar@gmail.com>
2025-01-03 21:36:50 +0530

    db

--- lib/db.ts
+++ lib/db.ts
@@ -2,12 +2,22 @@ import { PrismaClient } from '@prisma/client'
 import { PrismaNeon } from '@prisma/adapter-neon'
 import { Pool } from '@neondatabase/serverless'
 
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
+
 const connectionString = process.env.DATABASE_URL!
 const pool = new Pool({ connectionString })
 
-const prisma = new PrismaClient({
-  adapter: new PrismaNeon(pool)
-})
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    adapter: new PrismaNeon(pool)
+  })
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 export default prisma
 
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
